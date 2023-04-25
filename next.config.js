c4cc2088434779a74050c6a88ff785b338a7c73e
Diego Sagredo <dsagredog@gmail.com>
2023-04-25 19:35:42 -0400

    feat(config): config node

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ['localhost', 'bostami-next.vercel.app'],
+        domains: ['localhost'],
     },
     basePath: '/dsagredog',
     assetPrefix: '/dsagredog',
