54a19e58cf5a13485c28a771339c178d689b37c9
Diego Sagredo <dsagredog@gmail.com>
2023-06-12 17:34:52 -0400

    feat(download): add cv

---
+++ src/pages/api/info.js
@@ -0,0 +1,69 @@
+const info = {
+    about: {
+        title: '<ACERCA DE MÍ>',
+        paragraph1:
+            'Mi objetivo como desarrollador es seguir mejorando mis habilidades tanto técnicas como blandas, y formar parte de un equipo laboral con las mismas expectativas o aún mejores. Estoy siempre dispuesto a aprender y capacitarme en nuevas tecnologías.',
+        paragraph2:
+            'Además, me encuentro actualmente en proceso de aprendizaje para poder crecer aún más como desarrollador. Si está buscando un desarrollador Full Stack con habilidades sólidas en diversas tecnologías, comprometido con el trabajo en equipo y la mejora continua, estoy seguro de que sería una excelente incorporación a su equipo.',
+    },
+    technology: {
+        title: '<TECNOLOGÍAS>',
+        lang: [
+            {
+                id: 1,
+                name: 'javascript',
+            },
+            {
+                id: 2,
+                name: 'reactjs',
+            },
+            {
+                id: 3,
+                name: 'bootstrap',
+            },
+            {
+                id: 4,
+                name: 'nextjs',
+            },
+            {
+                id: 5,
+                name: 'typescript',
+            },
+            {
+                id: 6,
+                name: 'mongodb',
+            },
+            {
+                id: 7,
+                name: 'nodejs',
+            },
+            {
+                id: 8,
+                name: 'tailwind',
+            },
+        ],
+    },
+    tools: {
+        title: '<HERRAMIENTAS>',
+        lang: [
+            {
+                id: 1,
+                name: 'vscode',
+            },
+            {
+                id: 2,
+                name: 'git',
+            },
+            {
+                id: 3,
+                name: 'gitlab',
+            },
+            {
+                id: 4,
+                name: 'postman',
+            },
+        ],
+    },
+};
+
+export default info;
